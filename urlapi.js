4b27b820b22ea75b6e1a9f223dbb9ed05f6159a5
jafacorp <56782606+Francis6-precisio@users.noreply.github.com>
2021-11-30 12:58:17 +0100

    update now

--- urlapi.js
+++ urlapi.js
@@ -2,7 +2,7 @@
  * Created by sinclaireric on 14/05/2018.
  */
  
-  export const URL = 'https://api.monsieurtravaux.ci';
+ export const URL = 'https://api.monsieurtravaux.ci';
 
 
-/* export const URL = 'http://localhost:5000'; */
+ /* export const URL = 'http://localhost:5000'; */
